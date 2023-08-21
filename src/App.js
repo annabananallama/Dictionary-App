14bd3f69d8f796ce065a6cb9727d31566741da7b
annabananallama <annasimonelarsen@gmail.com>
2023-08-21 21:33:57 +0100

    Added search bar functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import "./styles.css";
+import SearchBar from "./searchbar.js";
 
 export default function App() {
   return (
     <div>
-      <h1> Dictionary App</h1>
+      <h1>Dictionary App</h1>
+      <SearchBar />
     </div>
   );
 }
---
+++ src/searchbar.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export default function SearchBar() {
+  const [word, setWord] = useState("");
+
+  function handleWordChange(event) {
+    setWord(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`Searching for ${word}`);
+  }
+
+  function handleKeyPress(event) {
+    if (event.key === "Enter") {
+      handleSubmit(event);
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter word here"
+          value={word}
+          onChange={handleWordChange}
+        />
+        <button type="submit">Enter</button>
+      </form>
+    </div>
+  );
+}
