45f0c84a5047d6148251488f37a83d067f9d0270
annabananallama <annasimonelarsen@gmail.com>
2023-09-03 21:32:04 +0100

    Added photo component and api

---
+++ src/Photos.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <div className="container">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <img src={photo.src.landscape} alt="" className="img-fluid" />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
--- src/searchbar.js
+++ src/searchbar.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function SearchBar() {
   const [word, setWord] = useState("");
   const [results, setResults] = useState(null);
+  const [photos, setPhotos] = useState(null);
 
   function handleWordChange(event) {
     setWord(event.target.value);
+  }
+
+  function handleImageResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function handleSubmit(event) {
@@ -15,6 +21,13 @@ export default function SearchBar() {
     if (word) {
       let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
       axios.get(apiUrl).then(handleResponse);
+
+      let imagekey = "9ceo40387d684fd72301c1f33tae08b2";
+      let imageApiUrl = `https://api.shecodes.io/images/v1/search?query=${word}&key=${imagekey}&per_page=1`;
+
+      axios.get(imageApiUrl).then(handleImageResponse);
+      let headers = { Authorization: `Bearer ${imagekey}` };
+      axios.get(imageApiUrl, { headers: headers }).then(handleImageResponse);
     }
   }
 
@@ -40,6 +53,7 @@ export default function SearchBar() {
         </form>
       </div>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
