1ed025ff73096eb437a47c0662dce3c4aba1873c
annabananallama <annasimonelarsen@gmail.com>
2023-09-03 22:13:52 +0100

    Added footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles.css";
 import SearchBar from "./searchbar.js";
+import Footer from "./Footer.js";
 
 export default function App() {
   return (
     <div className="content">
       <h1>DICTIONARY APP</h1>
       <SearchBar />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <a
+        href="https://github.com/annabananallama/Dictionary-App"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <strong>Open source code </strong>
+      </a>
+      by Anna Larsen
+    </div>
+  );
+}
